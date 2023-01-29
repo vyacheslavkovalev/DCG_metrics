d5d23d148d8ac5cdf8c206a69052753bbcd6eca2
Viacheslav Kovalev <slava@Viacheslavs-MacBook-Pro.local>
2023-01-29 15:07:13 +0300

    add script for CG@k

---
+++ CG@k.py
@@ -0,0 +1,23 @@
+from typing import List
+
+import numpy as np
+
+
+def cumulative_gain(relevance: List[float], k: int) -> float:
+    """Score is cumulative gain at k (CG@k)
+
+    Parameters
+    ----------
+    relevance:  `List[float]`
+        Relevance labels (Ranks)
+    k : `int`
+        Number of elements to be counted
+
+    Returns
+    -------
+    score : float
+    """
+    if k > len(relevance):
+        raise IndexError()
+    score = np.sum(relevance[:k])
+    return score
